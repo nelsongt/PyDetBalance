e4a9ef44d1c6fce1700ffe982f4f418e9205b6bc
George <deft@archlinux.us>
2015-01-30 19:19:35 -0500

    flux almost found

--- pydetbal.py
+++ pydetbal.py
@@ -1,49 +1,35 @@
+from __future__ import division
 import os
 import math
+import numpy as np
 import scipy.constants as sc
 import matplotlib.pyplot as plt
+from scipy import integrate
 
-print "Enter the (relative) location of the files: "; directory = raw_input()
 
-planck_ev = 4.135667516 #eV*s
-subtend_angle_degrees = 0.26 #degrees
+## Some Constants ##
+
+T_s = 6000 # K
+T_c = 300 # K
+k_B = 8.6173324E-05 # eV/K
+planck_ev = 4.135667516E-15 # eV*s
+subtend_angle_degrees = 0.26 # degrees
+
 geometrical_factor = math.pi * math.sin(math.radians(subtend_angle_degrees)) ** 2
 
 flux_coeff = 2 * geometrical_factor / (planck_ev**3 * sc.c**2)
 
-path = r"%s" % directory
+## Integrate to find the flux ##
 
-pathout = os.path.join(path, "jv")
-if not os.path.exists(pathout):
-  os.makedirs(pathout)
+planck_rad_func = lambda E: E**3 / (math.exp(E/(k_B*T_s)) - 1)
+flux, flux_err = integrate.quad(planck_rad_func,0,10) # Integrate the planck radiation function from 0 eV to 10 eV (~inf)
 
-for file in os.listdir(path):
-    current_file = os.path.join(path, file)
-    print "Current file: %s" % file
-    
-    f = open(current_file, "r")
-    data = [item for item in list(csv.reader(f))]
-    f.close()
-    
-    
-    new_data = [["Voltage (V)","Current Density (A/cm^2)"]]
-     
-    for i, item in enumerate(data):
-	if data[i][0] == "MetaData":
-	  if data[i][1] == " TestRecord.TestTarget":
-	    device = data[i][2].replace(" ","")
-	  elif data[i][1] == " TestRecord.Remarks":
-	    diode = data[i][2]
-	elif data[i][0] == "Dimension1":
-	  dimension = int(data[i][1].replace(" ",""))
-	  print "Data set found! Number of data points: %d" % dimension
-	  for x in xrange(i+3,i+3+dimension):
-	      new_data.append([data[x][1].replace(" ",""), float(data[x][2].replace(" ","")) / area])
-	  break
-	
-    output_file = os.path.join(pathout, "%s%s.csv" %(device, diode))
-    f = open(output_file, 'w')
-    csv.writer(f).writerows(new_data)
-    f.close()
 
-    print "File completed"
+for x in xrange(1,20):
+  y = (x/10)**2 / (math.exp((x/10)/(k_B*T_s)) - 1)
+  print "Y: %g" %y
+
+
+plt.plot(x, y, 'o', c = 'b')
+
+print "Flux: %g" %flux
